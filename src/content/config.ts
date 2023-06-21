1ed90b331382b065cb113d1856f8160a47c85461
Fercho5656 <morenomoranfernando@gmail.com>
2023-06-20 20:37:47 -0500

    Change img_url type to image

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const projectsCollection = defineCollection({
-	schema: z.object({
+	schema: ({image}) => z.object({
 		inProgress: z.boolean(),
 		title: z.string(),
 		tags: z.array(z.string()),
 		link: z.string(),
 		project_link: z.string().optional(),
-		img_url: z.string().optional(),
+		img_url: image(),
 		img_alt: z.string().optional(),
 	}),
 });
